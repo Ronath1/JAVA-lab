21a0450636f7b7527af6eb1161f6a0838afb7754
Ronath1 <140684168+Ronath1@users.noreply.github.com>
2023-07-30 11:18:06 +0530

    Add files via upload

---
+++ LAB DAY 11/Lab_Day_11_GUI/src/main/java/com/mycompany/lab_day_11_gui/Lab_Day_11_GUI.java
@@ -0,0 +1,10 @@
+
+package com.mycompany.lab_day_11_gui;
+
+public class Lab_Day_11_GUI {
+
+    public static void main(String[] args) {
+        ColorChangeApplication c1 = new ColorChangeApplication();
+        c1.show();
+    }
+}
---
+++ LAB DAY 11/Lab_Day_11_Q2_GUI/src/main/java/com/mycompany/lab_day_11_q2_gui/Lab_Day_11_Q2_GUI.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab_day_11_q2_gui;
+
+/**
+ *
+ * @author kanis
+ */
+public class Lab_Day_11_Q2_GUI {
+
+    public static void main(String[] args) {
+        CountApplication c2 = new CountApplication();
+        c2.show();
+    }
+}
